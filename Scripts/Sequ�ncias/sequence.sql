3fff74ca6b51df0ce87f68e540408ac1cbd91c20
icorreia <icorreia@student.dei.uc.pt>
2010-12-24 02:45:48 +0000

    Cenas

--- Scripts/Sequ�ncias/sequence.sql
+++ Scripts/Sequ�ncias/sequence.sql
@@ -1,6 +1,15 @@
+-- Sequences for people
 DROP SEQUENCE seq_id_pessoa;
 
 CREATE SEQUENCE seq_id_pessoa
+START WITH 1
+INCREMENT BY 1
+NOCYCLE;
+
+-- Sequences for books
+DROP SEQUENCE seq_id_aluguer;
+
+CREATE SEQUENCE seq_id_aluguer
 START WITH 1
 INCREMENT BY 1
 NOCYCLE;
--- Scripts/pl SQL/procedures.sql
+++ Scripts/pl SQL/procedures.sql
@@ -1,3 +1,4 @@
+-- Adds a new reader to the data base.
 CREATE OR REPLACE PROCEDURE addReader ( nomePessoa IN Pessoa.nome_pessoa%type, morada IN Pessoa.morada%type, bi IN NUMBER,telefone IN NUMBER, email IN Pessoa.e_mail%type) IS
 	current_id NUMBER;
 	
@@ -10,7 +11,14 @@ BEGIN
 		
 	COMMIT;
 	
+EXCEPTION
+	WHEN DUP_VAL_ON_INDEX THEN
+		current_id := -1;
+	WHEN OTHERS THEN
+		current_id := -1;
+		
 END;
+/
 
 
 CREATE OR REPLACE PROCEDURE addEmployee ( nomePessoa IN Pessoa.nome_pessoa%type, morada IN Pessoa.morada%type, bi IN NUMBER,telefone IN NUMBER, email IN Pessoa.e_mail%type) IS
@@ -25,4 +33,54 @@ BEGIN
 		
 	COMMIT;
 	
+EXCEPTION
+	WHEN DUP_VAL_ON_INDEX THEN
+		current_id := -1;
+		-- Error raised when we insert the same BI.
+		--TODO:  Return an appropriate value. Maybe we have to create a OUT variable
+	WHEN OTHERS THEN
+		--TODO: The same as above.
+		current_id := -1;
+	
 END;
+
+
+-- Script used to make requisitions.
+CREATE OR REPLACE PROCEDURE newRequisition ( book_id IN Publicacao.id_doc%type, reader_id IN Pessoa.id_pessoa%type, employee_id IN Pessoa.id_pessoa%type) IS
+	no_available INTEGER; 
+	id_pessoa_conf INTEGER; 
+	current_id NUMBER;
+	
+	ID_NOT_FOUND exception; 
+	PRAGMA exception_init(ID_NOT_FOUND, -2291); 
+	
+BEGIN 
+	SELECT seq_id_aluguer.nextval INTO current_id
+	FROM dual;
+	
+	SELECT p.disponiveis INTO no_available 
+	FROM Publicacao p 
+	WHERE p.id_doc = book_id; 
+	
+	IF (no_available > 0) THEN 
+		UPDATE Publicacao p SET p.disponiveis = p.disponiveis - 1 
+		WHERE p.id_doc = book_id; 
+		
+		UPDATE Leitor SET no_emprestimos = no_emprestimos + 1
+		WHERE id_pessoa = reader_id;
+		
+		INSERT INTO Emprestimo VALUES (reader_id, employee_id, book_id, current_id, SYSDATE, SYSDATE + 7, null ); 
+		 
+	ELSE 
+		id_pessoa_conf := -1; 
+	END IF; 
+	
+	COMMIT; 
+	
+EXCEPTION 
+	WHEN NO_DATA_FOUND THEN 
+		id_pessoa_conf := -1; 
+	WHEN ID_NOT_FOUND THEN 
+		id_pessoa_conf := -1; 
+
+END; 
